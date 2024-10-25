04cdbb0bc545d3fa48dc567fac80dfd77f46addd
ricardo <ricardo.billboardplanet@gmail.com>
2024-10-24 23:08:34 -0600

    Agregar archivos de proyecto.

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public required string Name { get; set; }
+        public int  Age { get; set; }
+        public required string Email { get; set; }
+
+    }
+}
