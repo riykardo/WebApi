5d2aeb7cb08b257185003f1cc184b228dd45a3ea
ricardo <ricardo.billboardplanet@gmail.com>
2024-10-25 19:47:29 -0600

    wep appi INIt

---
+++ Context/DataBaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Context
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
